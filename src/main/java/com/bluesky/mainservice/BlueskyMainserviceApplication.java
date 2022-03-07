d37b71754aa2ad6f85b7bc14920c51c65a7c810f
shawnlsj <hortensia08@naver.com>
2022-03-07 15:10:16 +0900

    test

---
+++ src/main/java/com/bluesky/mainservice/BlueskyMainserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bluesky.mainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueskyMainserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueskyMainserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluesky/mainservice/BlueskyMainserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluesky.mainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueskyMainserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
