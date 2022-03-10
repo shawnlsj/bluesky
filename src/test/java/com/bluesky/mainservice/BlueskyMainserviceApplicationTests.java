f60cae68a69a93f15f16c54c8dd4f5dce16a5e5d
shawnlsj <hortensia08@naver.com>
2022-03-11 01:17:34 +0900

    cdn 깃 이그노어 추가 cdn 폴더에 부트스트랩 추가

--- src/test/java/com/bluesky/mainservice/BlueskyMainserviceApplicationTests.java
+++ src/test/java/com/bluesky/mainservice/BlueskyMainserviceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.bluesky.mainservice;
 
+import com.bluesky.mainservice.entity.Hello;
+import com.bluesky.mainservice.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceUnitUtil;
+import javax.transaction.Transactional;
+
+@Transactional
 @SpringBootTest
 class BlueskyMainserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
