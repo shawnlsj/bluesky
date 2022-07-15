0712c5527b51e9ef91e6f132034504414f09183a
shawnlsj <hortensia08@naver.com>
2022-07-16 02:26:33 +0900

    add: refer 체크 필터, https 검사 필터 추가

--- src/main/java/com/bluesky/mainservice/config/WebConfig.java
+++ src/main/java/com/bluesky/mainservice/config/WebConfig.java
@@ -1,8 +1,13 @@
 package com.bluesky.mainservice.config;
 
-import com.bluesky.mainservice.config.argumentresolver.MobilePageArgumentResolver;
+import com.bluesky.mainservice.config.filter.RefererCheckFilter;
+import com.bluesky.mainservice.config.interceptor.AdminUrlInterceptor;
+import com.bluesky.mainservice.config.interceptor.ResolveViewModeInterceptor;
+import com.bluesky.mainservice.controller.argument.argumentresolver.LoginUserArgumentResolver;
+import com.bluesky.mainservice.controller.argument.argumentresolver.MobilePageArgumentResolver;
+import com.bluesky.mainservice.config.filter.HttpsValidationFilter;
 import com.bluesky.mainservice.config.filter.XssFilter;
-import com.bluesky.mainservice.config.interceptor.XssInterceptor;
+import com.bluesky.mainservice.config.interceptor.FilteredXssCheckInterceptor;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -20,27 +25,57 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
         resolvers.add(new MobilePageArgumentResolver());
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new XssInterceptor())
+        registry.addInterceptor(new AdminUrlInterceptor())
+                .order(0)
+                .addPathPatterns("/admin/**");
+
+        registry.addInterceptor(new FilteredXssCheckInterceptor())
                 .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/js/**");
+
+        registry.addInterceptor(new ResolveViewModeInterceptor())
+                .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/js/**");
     }
 
     @Bean
-    public FilterRegistrationBean xssFilter(){
+    public FilterRegistrationBean<Filter> refererCheckFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new XssFilter());
+        filterRegistrationBean.setFilter(new RefererCheckFilter());
         filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(Integer.MIN_VALUE);
+        return filterRegistrationBean;
+    }
+    
+    @Bean
+    public FilterRegistrationBean<Filter> httpsValidationFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new HttpsValidationFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(Integer.MIN_VALUE + 1);
         return filterRegistrationBean;
     }
 
     @Bean
-    public JPAQueryFactory jpaQueryFactory(EntityManager em){
+    public FilterRegistrationBean<Filter> xssFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new XssFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+
+    //Querydsl 라이브러리에서 사용할 빈 등록
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory(em);
     }
 }
