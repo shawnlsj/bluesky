01d4bebaebb09c426cebd2f815c16dbbd2b2b0f9
shawnlsj <hortensia08@naver.com>
2022-07-19 11:12:00 +0900

    add: UserResponseDto 추가

--- src/main/java/com/bluesky/mainservice/controller/user/dto/UserResponseDto.java
+++ src/main/java/com/bluesky/mainservice/controller/user/dto/UserResponseDto.java
@@ -1,16 +1,29 @@
 package com.bluesky.mainservice.controller.user.dto;
 
+import com.bluesky.mainservice.repository.user.constant.AccountType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 public class UserResponseDto {
 
+    @Builder
+    @Getter
+    public static class UserAccount {
+        private final String maskedEmail;
+        private final String nickname;
+        private final String accountType;
+        private final LocalDateTime joinDate;
+    }
+
     @Getter
     public static class JoinUserInfo {
-        String token;
-        String maskedEmail;
-        boolean isOriginalUser;
-        String joinUrl;
+        private final String token;
+        private final String maskedEmail;
+        private final boolean isOriginalUser;
+        private final String joinUrl;
 
         public JoinUserInfo(String token, String maskedEmail, boolean isOriginalUser) {
             this.token = token;
@@ -27,12 +40,12 @@ public class UserResponseDto {
     @AllArgsConstructor
     @Getter
     public static class SendEmailResult {
-        boolean isSuccess;
+        private final boolean isSuccess;
     }
 
     @AllArgsConstructor
     @Getter
     public static class CheckNicknameResult {
-        boolean isAvailable;
+        private final boolean isAvailable;
     }
 }
