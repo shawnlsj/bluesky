52a909c111f205c084ee7855a21ac02fe2212e9d
shawnlsj <hortensia08@naver.com>
2022-07-19 14:15:19 +0900

    update: @EnableJpaAuditing 추가

--- src/main/java/com/bluesky/mainservice/BlueskyMainserviceApplication.java
+++ src/main/java/com/bluesky/mainservice/BlueskyMainserviceApplication.java
@@ -2,9 +2,9 @@ package com.bluesky.mainservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Locale;
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class BlueskyMainserviceApplication {
 
