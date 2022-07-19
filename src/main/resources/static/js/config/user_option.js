562be52f483dd91409e0246aa0fedd1295d6014b
shawnlsj <hortensia08@naver.com>
2022-07-19 13:48:33 +0900

    add: user_option.js 추가
    닉네임 최대길이 등 사용자 프로퍼티 설정을 저장할 파일 추가

---
+++ src/main/resources/static/js/config/user_option.js
@@ -0,0 +1,7 @@
+export const MAX_EMAIL_LENGTH = 50;
+
+export const MIN_PASSWORD_LENGTH = 8;
+export const MAX_PASSWORD_LENGTH = 9999;
+
+export const MIN_NICKNAME_LENGTH = 2;
+export const MAX_NICKNAME_LENGTH = 16;
