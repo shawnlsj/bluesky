f0459c2dbb96219eb08e3d56f9f853b8d09b38b6
shawnlsj <hortensia08@naver.com>
2022-07-19 14:17:18 +0900

    update: encryptor 타입을 PooledPBEStringEncryptor 로 업데이트

--- src/main/java/com/bluesky/mainservice/config/security/JasyptConfig.java
+++ src/main/java/com/bluesky/mainservice/config/security/JasyptConfig.java
@@ -1,20 +1,23 @@
-package com.bluesky.mainservice.config;
+package com.bluesky.mainservice.config.security;
 
 import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
-public class EncryptConfig {
+public class JasyptConfig {
 
     @Value("${jasypt.encryptor.password}")
     private String password;
 
+    @Primary
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor(){
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setPoolSize(4);
         encryptor.setAlgorithm("PBEWithMD5AndDES");
         encryptor.setPassword(password);
         return encryptor;
