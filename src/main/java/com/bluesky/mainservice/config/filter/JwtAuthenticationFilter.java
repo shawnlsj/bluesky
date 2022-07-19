c3a9447bea8e7d4a79df91bff1ad4c7d9d3f7a7f
shawnlsj <hortensia08@naver.com>
2022-07-19 11:32:55 +0900

    add: jwt 인증 필터와 인증을 진행할 클래스 추가
    JwtAuthenticationFilter -> 시큐리티 필터체인에서 인증 메소드를 호출하는 필터
    JwtAuthenticationProvider -> 인증 메소드를 구현하고 있는 클래스

---
+++ src/main/java/com/bluesky/mainservice/config/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.bluesky.mainservice.config.filter;
+
+import com.bluesky.mainservice.config.security.jwt.JwtAuthenticationProvider;
+import com.bluesky.mainservice.config.security.jwt.JwtMapper;
+import com.bluesky.mainservice.config.security.jwt.JwtGenerator;
+import com.bluesky.mainservice.service.user.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+import static com.bluesky.mainservice.config.security.jwt.TokenType.ACCESS;
+import static com.bluesky.mainservice.config.security.jwt.TokenType.REFRESH;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final JwtGenerator jwtGenerator;
+    private final LoginService loginService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = JwtMapper.resolveAccessToken(request);
+        String refreshToken = JwtMapper.resolveRefreshToken(request);
+
+        //유효한 액세스 토큰이면 인증 진행
+        //유효하지 않으면 리프레시 토큰으로 액세스 토큰 발급 시도
+        if (StringUtils.hasText(accessToken) && jwtGenerator.isValid(accessToken, ACCESS)) {
+            SecurityContextHolder.getContext().setAuthentication(jwtAuthenticationProvider.authenticate(accessToken));
+
+            //리프레시 토큰으로 액세스 토큰이 발급 되었으면 인증 진행
+            //발급이 안되었으면 토큰을 삭제
+        } else if (StringUtils.hasText(refreshToken) && jwtGenerator.isValid(refreshToken, REFRESH)) {
+            Optional<String> refreshedAccessToken = loginService.refreshAccessToken(refreshToken);
+            if (refreshedAccessToken.isEmpty()) {
+                JwtMapper.removeAccessToken(response);
+                JwtMapper.removeRefreshToken(response);
+            } else {
+                accessToken = refreshedAccessToken.get();
+                SecurityContextHolder.getContext().setAuthentication(jwtAuthenticationProvider.authenticate(accessToken));
+                JwtMapper.addAccessToken(response, accessToken);
+            }
+        }
+        doFilter(request, response, filterChain);
+    }
+}
