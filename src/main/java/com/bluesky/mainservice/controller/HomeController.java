359b98c9bf0aa9a1a550beeebf184be868b36f9a
shawnlsj <hortensia08@naver.com>
2022-07-19 11:55:10 +0900

    update: HomeController 에서 로그인 사용자 정보를 매개변수로 받음

--- src/main/java/com/bluesky/mainservice/controller/HomeController.java
+++ src/main/java/com/bluesky/mainservice/controller/HomeController.java
@@ -1,18 +1,35 @@
 package com.bluesky.mainservice.controller;
 
-import com.bluesky.mainservice.controller.argument.MobilePage;
+import com.bluesky.mainservice.controller.argument.LoginUser;
+import com.bluesky.mainservice.util.CookieUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping("/")
 @Controller
 public class HomeController {
 
     @GetMapping
-    public String home(MobilePage mobilePage) {
-        if (mobilePage.isRequested()) {
-            return "index_m";
+    public String home(HttpServletRequest request, HttpServletResponse response, LoginUser loginUser, Model model) {
+
+        if (loginUser != null) {
+            model.addAttribute("isLoginUser", true);
+        }
+
+        //쿠키에서 화면에 출력할 메시지를 획득
+        String message = CookieUtils.resolveMessageCookie(request);
+        if (StringUtils.hasText(message)) {
+            //메시지를 획득한 쿠키를 삭제
+            CookieUtils.removeMessageCookie(response);
+
+            //화면으로 메시지를 전달
+            model.addAttribute("message", message);
         }
         return "index";
     }
