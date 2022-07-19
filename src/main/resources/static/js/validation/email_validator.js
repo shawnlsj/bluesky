e77755bccd836b77ce4777c515f22ef3b1ef42d0
shawnlsj <hortensia08@naver.com>
2022-07-19 12:03:41 +0900

    add: js 이메일 검증기 추가

---
+++ src/main/resources/static/js/validation/email_validator.js
@@ -0,0 +1,29 @@
+import {MAX_EMAIL_LENGTH} from "/js/config/option.js";
+
+class EmailValidator {
+    static emailForm = document.getElementById("email");
+
+    static isValid(email){
+        const EMAIL_VALIDATION_REGEX = /^[^@]+@[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])*(\.[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])*)*(\.[a-zA-Z][a-zA-Z0-9-]*[a-zA-Z0-9])$/;
+        return email.match(EMAIL_VALIDATION_REGEX) && email.length <= MAX_EMAIL_LENGTH;
+    }
+
+    static initEventListener() {
+        const emailForm = this.emailForm;
+
+        if (emailForm === null) {
+            return;
+        }
+
+        emailForm.addEventListener("keyup", function () {
+            const email = emailForm.value;
+            if (EmailValidator.isValid(email)) {
+                enableValid(emailForm);
+            } else {
+                enableInvalid(emailForm);
+            }
+        });
+    }
+}
+
+export {EmailValidator};
