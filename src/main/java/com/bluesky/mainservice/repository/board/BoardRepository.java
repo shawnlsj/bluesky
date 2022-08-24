54a0ea72571574189bba80a5f67d861bc7104da4
shawnlsj <hortensia08@naver.com>
2022-08-24 16:26:56 +0900

    add: BoardRepository 추가

--- src/main/java/com/bluesky/mainservice/repository/board/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bluesky.mainservice.repository.board;
-
-import com.bluesky.mainservice.repository.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
-
-}
