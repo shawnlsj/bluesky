5c7d8c941b6c1e1656cdcb73567bca13da39b938
shawnlsj <hortensia08@naver.com>
2022-08-24 16:16:40 +0900

    refactor: UserOption static 변수 순서 변경

--- src/main/java/com/bluesky/mainservice/controller/user/constant/UserOption.java
+++ src/main/java/com/bluesky/mainservice/controller/user/constant/UserOption.java
@@ -2,14 +2,14 @@ package com.bluesky.mainservice.controller.user.constant;
 
 public class UserOption {
 
+    //이메일 최대 길이
+    public static final int MAX_EMAIL_LENGTH = 50;
+
     //닉네임 최대 글자 수
     public static final int MAX_NICKNAME_LENGTH = 16;
 
     //닉네임 최소 글자 수
     public static final int MIN_NICKNAME_LENGTH = 2;
-
-    //이메일 최대 길이
-    public static final int MAX_EMAIL_LENGTH = 50;
 
     //비밀번호 최소 길이
     public static int MIN_PASSWORD_LENGTH = 8;
