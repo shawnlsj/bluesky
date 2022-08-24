709231feba26cc7b5e3266837e4c4fbd2d85732b
shawnlsj <hortensia08@naver.com>
2022-08-24 13:19:55 +0900

    update: xss 필터가 검사 대상 url 에서만 동작하도록 수정

--- src/main/java/com/bluesky/mainservice/config/filter/XssFilter.java
+++ src/main/java/com/bluesky/mainservice/config/filter/XssFilter.java
@@ -7,32 +7,38 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 public class XssFilter extends XssEscapeServletFilter {
 
-    private static final List excludeUrlList = new ArrayList();
+    private static final List<String> targetPathList = new ArrayList<>();
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        excludeUrlList.add("/css");
-        excludeUrlList.add("/js");
-        super.init(filterConfig);
+    public void init(FilterConfig filterConfig){
+        targetPathList.add("/board");
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String url = ((HttpServletRequest) request).getRequestURI();
-        if (isExcludeUrl(url)) {
-            chain.doFilter(request, response);
-        } else {
-            log.debug("url : {}", url);
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String url = httpServletRequest.getRequestURI();
+        String method = httpServletRequest.getMethod();
+
+        //필터링 대상 url 일 경우와 리소스 변경 메소드일 경우만 xss 필터링 진행
+        if (startWithTargetPath(url) && anyMatch(method, "POST", "PUT", "PATCH")) {
             super.doFilter(request, response, chain);
+        } else {
+            chain.doFilter(request, response);
         }
     }
 
-    private boolean isExcludeUrl(String url) {
-        return excludeUrlList.stream().anyMatch(excludeUrl -> url.startsWith((String) excludeUrl));
+    private boolean startWithTargetPath(String url) {
+        return targetPathList.stream().anyMatch(url::startsWith);
+    }
+
+    private boolean anyMatch(String target, String... args) {
+        return Arrays.asList(args).contains(target);
     }
 }
