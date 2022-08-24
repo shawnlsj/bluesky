952e9d8b114e3aa27127fff24e7ce6ff7ef8289e
shawnlsj <hortensia08@naver.com>
2022-08-24 16:23:49 +0900

    update: RoleType 변수 추가, 모든 어드민을 반환하는 메서드 추가

--- src/main/java/com/bluesky/mainservice/repository/user/constant/RoleType.java
+++ src/main/java/com/bluesky/mainservice/repository/user/constant/RoleType.java
@@ -1,5 +1,9 @@
 package com.bluesky.mainservice.repository.user.constant;
 
 public enum RoleType {
-    ADMIN, USER
+    ADMIN, ADMIN_PROGRAMMING, ADMIN_ENGLISH, ADMIN_TRAVEL, USER,;
+
+    public static RoleType[] allAdmins() {
+        return new RoleType[]{ADMIN, ADMIN_PROGRAMMING, ADMIN_PROGRAMMING, ADMIN_TRAVEL};
+    }
 }
