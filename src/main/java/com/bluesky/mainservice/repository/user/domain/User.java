ced45d88f1d7042d5c4122ca1a7444cb78f9c033
shawnlsj <hortensia08@naver.com>
2022-08-24 23:16:39 +0900

    update: user 엔티티 profile_image 컬럼 추가

--- src/main/java/com/bluesky/mainservice/repository/user/domain/User.java
+++ src/main/java/com/bluesky/mainservice/repository/user/domain/User.java
@@ -3,10 +3,8 @@ package com.bluesky.mainservice.repository.user.domain;
 import com.bluesky.mainservice.repository.BaseTimeEntity;
 import com.bluesky.mainservice.repository.user.constant.AccountType;
 import com.bluesky.mainservice.repository.user.constant.RoleType;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Hibernate;
 import org.springframework.util.StringUtils;
 
@@ -16,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -24,15 +23,6 @@ import java.util.UUID;
         @Index(name = "uuid", columnList = "uuid"),
         @Index(name = "email", columnList = "email")} )
 public class User extends BaseTimeEntity {
-
-    @Builder
-    public User(String socialLoginId, String email, String password, String nickname, AccountType accountType) {
-        this.socialLoginId = socialLoginId;
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
-        this.accountType = accountType;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -44,7 +34,7 @@ public class User extends BaseTimeEntity {
 
     @NotNull
     @Column(columnDefinition = "binary(16)", unique = true)
-    private UUID uuid = UUID.randomUUID();
+    private UUID uuid;
 
     @NotNull
     private String email;
@@ -54,6 +44,9 @@ public class User extends BaseTimeEntity {
     @Column(unique = true)
     private String nickname;
 
+    @Column(name = "profile_image")
+    private String profileImage;
+
     @OneToMany(mappedBy = "user")
     private List<UserRole> userRoles = new ArrayList<>();
 
@@ -61,6 +54,31 @@ public class User extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "account_type")
     private AccountType accountType;
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    @Builder
+    private User(String socialLoginId,
+                 String email,
+                 String password,
+                 String nickname,
+                 @NonNull AccountType accountType) {
+        this.socialLoginId = socialLoginId;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.accountType = accountType;
+        uuid = UUID.randomUUID();
+    }
+
+    @PostLoad
+    private void initRole() {
+        log.info("사용자 권한 정보 초기화");
+        Hibernate.initialize(userRoles);
+        Hibernate.initialize(userRoles.get(0).getRole());
+    }
 
     public void updateEmail(String email) {
         this.email = email;
@@ -74,10 +92,8 @@ public class User extends BaseTimeEntity {
         this.nickname = nickname;
     }
 
-    @PostLoad
-    public void initRole() {
-        Hibernate.initialize(userRoles);
-        Hibernate.initialize(userRoles.get(0).getRole());
+    public void updateProfileImage(String profileImage) {
+        this.profileImage = profileImage;
     }
 
     public boolean isRegisteredUser() {
