2fa83555f774ab75786367734668b92fa0869ab4
shawnlsj <hortensia08@naver.com>
2022-08-24 22:04:15 +0900

    update: 닉네임, 패스워드 개행, 탭 문자 검증 로직 추가

--- src/main/java/com/bluesky/mainservice/controller/user/validation/PasswordValidator.java
+++ src/main/java/com/bluesky/mainservice/controller/user/validation/PasswordValidator.java
@@ -1,4 +1,4 @@
-package com.bluesky.mainservice.controller.validation;
+package com.bluesky.mainservice.controller.user.validation;
 
 import com.bluesky.mainservice.util.RegexUtils;
 
@@ -13,6 +13,12 @@ public class PasswordValidator implements ConstraintValidator<Password, String> 
     public boolean isValid(String value, ConstraintValidatorContext context) {
         //비밀번호의 길이가 올바른지 확인
         if (value.length() >= MIN_PASSWORD_LENGTH && value.length() <= MAX_PASSWORD_LENGTH) {
+
+            //탭, 개행, 공백 문자가 들어있으면 false
+            if (RegexUtils.contains(value, "\\t|\\R|\\s")) {
+                return false;
+            }
+
             //비밀번호에 영문/숫자/특수문자 중 몇 종류가 사용되었는지 카운트할 변수
             int matchCount = 0;
 
@@ -20,17 +26,17 @@ public class PasswordValidator implements ConstraintValidator<Password, String> 
             if (RegexUtils.isContainAlphabet(value)) {
                 matchCount++;
             }
+
             //숫자가 포함되어 있으면 카운트 +1
             if (RegexUtils.isContainNumber(value)) {
                 matchCount++;
             }
+
             //특수문자가 포함되어 있으면 카운트 +1
             if (RegexUtils.isContainSpecialChar(value)) {
                 matchCount++;
             }
-            if (matchCount >= 2) {
-                return true;
-            }
+            return matchCount >= 2;
         }
         return false;
     }
