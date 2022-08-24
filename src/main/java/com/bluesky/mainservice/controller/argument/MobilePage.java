16bbb22ef0a41b5bd4dec750084e4e01b54ebb80
shawnlsj <hortensia08@naver.com>
2022-08-24 17:14:51 +0900

    refactor: MobilePageArgument -> MobilePage 클래스명 변경

---
+++ src/main/java/com/bluesky/mainservice/controller/argument/MobilePage.java
@@ -0,0 +1,10 @@
+package com.bluesky.mainservice.controller.argument;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MobilePage {
+    private final boolean isRequested;
+}
