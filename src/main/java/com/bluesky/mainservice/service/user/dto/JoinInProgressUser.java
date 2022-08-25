5c5edeeecf8c368611a4998d0aa576c63d25046b
shawnlsj <hortensia08@naver.com>
2022-08-25 13:46:05 +0900

    refactor: 멤버 변수를 private final 로 변경

--- src/main/java/com/bluesky/mainservice/service/user/dto/JoinInProgressUser.java
+++ src/main/java/com/bluesky/mainservice/service/user/dto/JoinInProgressUser.java
@@ -1,16 +1,17 @@
 package com.bluesky.mainservice.service.user.dto;
 
 import com.bluesky.mainservice.repository.user.constant.AccountType;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 
 /**
  * 회원가입 진행중인 사용자에 대한 정보를 포함합니다.
  */
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
-public class JoinInProgressUserInfo {
-    String email;
-    AccountType accountType;
+public class JoinInProgressUser {
+
+    private final String email;
+    private final AccountType accountType;
 }
