17f38c11b29347aa12a2aba437dd1e42740735c3
shawnlsj <hortensia08@naver.com>
2022-08-25 12:35:19 +0900

    update: ApiExceptionHandler 처리 대상 예외 추가

--- src/main/java/com/bluesky/mainservice/controller/error/ApiExceptionHandler.java
+++ src/main/java/com/bluesky/mainservice/controller/error/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.bluesky.mainservice.controller.error;
 
-import com.bluesky.mainservice.controller.error.dto.ErrorInfo;
+import com.bluesky.mainservice.controller.community.board.BoardApiController;
+import com.bluesky.mainservice.controller.error.dto.Error;
 import com.bluesky.mainservice.controller.user.UserApiController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,20 +9,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = {UserApiController.class})
+import javax.validation.ValidationException;
+
+@RestControllerAdvice(assignableTypes = {UserApiController.class, BoardApiController.class})
 public class ApiExceptionHandler {
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ErrorInfo handleBeanValidationEx() {
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class, ValidationException.class})
+    public Error handleBeanValidationEx() {
         String message = "요청 값이 올바르지 않습니다.";
-        return new ErrorInfo(message);
+        return new Error(message);
     }
 
     @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler
-    public ErrorInfo handleError(Exception e) {
-        String message = "서버에 문제가 발생하였습니다.";
-        return new ErrorInfo(message);
+    public Error handleError(RuntimeException e) {
+        String message = "서버에 오류가 발생하였습니다.";
+        return new Error(message);
     }
 }
