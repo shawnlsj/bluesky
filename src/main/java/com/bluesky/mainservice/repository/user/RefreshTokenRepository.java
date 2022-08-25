e7a42f3e5694217552a16a5273e5c0b08f998b7c
shawnlsj <hortensia08@naver.com>
2022-08-25 15:44:05 +0900

    refactor: 메소드 사이에 개행 추가

--- src/main/java/com/bluesky/mainservice/repository/user/RefreshTokenRepository.java
+++ src/main/java/com/bluesky/mainservice/repository/user/RefreshTokenRepository.java
@@ -8,5 +8,6 @@ import java.util.UUID;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     RefreshToken findByUuid(UUID uuid);
+
     void deleteByUuid(UUID uuid);
 }
