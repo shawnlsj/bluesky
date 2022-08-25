7f1041b4d8386a8e0246fdd441d02ba88aba2002
shawnlsj <hortensia08@naver.com>
2022-08-25 15:57:33 +0900

    refactor: 메소드를 직접 호출하지 않고 클래스를 통해 호출하도록 변경

--- src/test/java/com/bluesky/mainservice/unit/PageNavigationTest.java
+++ src/test/java/com/bluesky/mainservice/unit/PageNavigationTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static com.bluesky.mainservice.controller.community.board.dto.BoardResponseDto.PageNavigation;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -21,8 +22,12 @@ class PageNavigationTest {
     @MethodSource("calculateStartPageNumParams")
     @DisplayName("calculateStartPageNum 메소드 테스트")
     void calculateStartPageNumTest(int requestedPageNum, int pageNavigationSize, int exptected) {
+        //given
+        PageNavigation pageNavigation =
+                new PageNavigation(requestedPageNum, Integer.MAX_VALUE, pageNavigationSize);
+
         //when
-        int result = calculateStartPageNum(requestedPageNum, pageNavigationSize);
+        int result = pageNavigation.getStartPageNum();
 
         //then
         assertThat(result).isEqualTo(exptected);
@@ -49,13 +54,16 @@ class PageNavigationTest {
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     @DisplayName("calculateEndPageNum 메소드 테스트")
     class CalculateEndPageNumTest {
-
         @ParameterizedTest(name = "{index}. 요청된 페이지: {0}, 네비게이션 크기: {1}, 총 페이지 수: {2}, 예상 결과값: {3}")
         @MethodSource
         @DisplayName("(총 페이지 수 >= 네비게이션의 끝 번호) 인 경우")
         void when_total_pages_ge_last_num_of_nav(int requestedPageNum, int pageNavigationSize, int totalPagesNum, int exptected) {
+            //given
+            PageNavigation pageNavigation =
+                    new PageNavigation(requestedPageNum, totalPagesNum, pageNavigationSize);
+
             //when
-            int result = calculateEndPageNum(requestedPageNum, pageNavigationSize, totalPagesNum);
+            int result = pageNavigation.getEndPageNum();
 
             //then
             assertThat(result).isEqualTo(exptected);
@@ -76,8 +84,12 @@ class PageNavigationTest {
         @MethodSource
         @DisplayName("(총 페이지 수 < 네비게이션의 끝 번호) 인 경우")
         void when_total_pages_lt_last_num_of_nav(int requestedPageNum, int pageNavigationSize, int totalPagesNum, int exptected) {
+            //given
+            PageNavigation pageNavigation =
+                    new PageNavigation(requestedPageNum, totalPagesNum, pageNavigationSize);
+
             //when
-            int result = calculateEndPageNum(requestedPageNum, pageNavigationSize, totalPagesNum);
+            int result = pageNavigation.getEndPageNum();
 
             //then
             assertThat(result).isEqualTo(exptected);
@@ -92,40 +104,6 @@ class PageNavigationTest {
                     arguments(5, 10, 9, 9),
                     arguments(11, 10, 15, 15),
                     arguments(22, 10, 22, 22));
-        }
-    }
-
-    private int calculateStartPageNum(int requestedPageNum, int pageNavigationSize) {
-        //요청한 페이지 번호를 페이지네비게이션 크기로 나눈 몫을 이용하여 계산식을 만들 때,
-        //나머지가 없는 경우는 페이지네비게이션 내의 다른 번호보다 몫이 1이 더 많으므로 분기를 나눈다
-        if (requestedPageNum % pageNavigationSize == 0) {
-            //예) 요청한 페이지 번호가 90번, 페이지네비게이션 크기가 10이라고 하면
-            //시작 번호는 81번이 되야 하므로 (몫 - 1) * 10 을 한 뒤 1을 더해준다
-            return ((requestedPageNum / pageNavigationSize) - 1) * pageNavigationSize + 1;
-        } else {
-            //예) 요청한 페이지 번호가 88번, 페이지 네이션 크기가 10이라고 하면
-            //시작 번호는 81번이 되야 하므로 몫 * 10 을 한 뒤 1을 더해준다
-            return (requestedPageNum / pageNavigationSize) * pageNavigationSize + 1;
-        }
-    }
-
-    private int calculateEndPageNum(int requestedPageNum, int pageNavigationSize, int totalPagesNum) {
-        //요청한 페이지 번호를 페이지네비게이션 크기로 나눈 몫을 이용하여 계산식을 만들 때,
-        //나머지가 없는 경우는 페이지네비게이션 내의 다른 번호보다 몫이 1이 더 많으므로 분기를 나눈다
-        if (requestedPageNum % pageNavigationSize == 0) {
-            //요청한 페이지 번호를 페이지네비게이션 크기로 나눈 나머지가 0이라는 것은
-            //요청한 페이지 번호가 네비게이션의 끝 번호라는 것을 의마한다
-            return requestedPageNum;
-        } else {
-            //예) 요청한 페이지 번호가 87, 페이지네비게이션 크기가 10이라고 하면
-            //마지막 번호는 90이 되야 하므로 (몫 + 1) * 10 을 한다
-            int endOfPageNavigationNum = ((requestedPageNum / pageNavigationSize) + 1) * pageNavigationSize;
-            if (totalPagesNum >= endOfPageNavigationNum) {
-                return endOfPageNavigationNum;
-            } else {
-                //총 페이지 수가 마지막 번호보다 작은 경우는 총 페이지 수가 마지막 번호가 된다
-                return totalPagesNum;
-            }
         }
     }
 }
