cc30385c17d947b22460fdd99b2425faefa79a39
shawnlsj <hortensia08@naver.com>
2022-08-25 13:35:28 +0900

    add: 확인 창 모달 추가

--- src/main/resources/static/js/util/modal.js
+++ src/main/resources/static/js/util/modal.js
@@ -1,9 +1,19 @@
 const successModalElement = document.getElementById("successModal");
 const successModalBody = document.getElementById("successModalBody");
 const successModal = initSuccessModal();
+
 const failModalElement = document.getElementById("failModal");
+const failModalTitle = document.getElementById("failModalTitle");
 const failModalBody = document.getElementById("failModalBody");
 const failModal = initFailModal();
+
+const confirmModalElement = document.getElementById("confirmModal");
+const confirmModalTitle = document.getElementById("confirmModalTitle");
+const confirmModalBody = document.getElementById("confirmModalBody");
+const confirmModalConfirmButton = document.getElementById("confirmModalConfirmButton");
+const confirmModalCancelButton = document.getElementById("confirmModalCancelButton");
+
+let confirmModal = initConfirmModal();
 
 function initSuccessModal() {
     if (successModalElement !== null) {
@@ -19,6 +29,13 @@ function initFailModal() {
     return null;
 }
 
+function initConfirmModal() {
+    if (confirmModalElement !== null) {
+        return new bootstrap.Modal(confirmModalElement);
+    }
+    return null;
+}
+
 (function init() {
     document.addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
@@ -27,6 +44,9 @@ function initFailModal() {
             }
             if (failModal !== null) {
                 failModal.hide();
+            }
+            if (confirmModal !== null) {
+                confirmModal.hide()
             }
         }
     });
@@ -40,6 +60,11 @@ function initFailModal() {
             document.activeElement.blur();
         });
     }
+    if (confirmModalElement !== null) {
+        confirmModalElement.addEventListener("shown.bs.modal", function () {
+            document.activeElement.blur();
+        });
+    }
 })();
 
 export function showSuccessModal(message) {
@@ -49,11 +74,31 @@ export function showSuccessModal(message) {
     successModal.show();
 }
 
-export function showFailModal(message) {
+export function showFailModal(message, title) {
     if (message !== undefined) {
         failModalBody.innerText = message;
     }
+    if (title !== undefined) {
+        failModalTitle.innerText = title;
+    }
     failModal.show();
+}
+
+export function showConfirmModal(message, title, callback) {
+    document.getElementById("confirmModalConfirmButton").remove();
+    const confirmButtonClone = confirmModalConfirmButton.cloneNode(true);
+    if (message !== undefined) {
+        confirmModalBody.innerText = message;
+    }
+    if (title !== undefined) {
+        confirmModalTitle.innerText = title;
+    }
+
+    confirmButtonClone.addEventListener("click", () => {
+        callback();
+    });
+    confirmModalCancelButton.after(confirmButtonClone);
+    confirmModal.show();
 }
 
 export {successModal, successModalBody, failModal, failModalBody};
