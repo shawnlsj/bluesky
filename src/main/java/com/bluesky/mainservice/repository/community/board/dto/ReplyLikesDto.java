5507dd9a53427a216956dd06552a214e87cf8d86
shawnlsj <hortensia08@naver.com>
2022-08-25 10:36:57 +0900

    add: 댓글 좋아요 추가

---
+++ src/main/java/com/bluesky/mainservice/repository/community/board/dto/ReplyLikesDto.java
@@ -0,0 +1,10 @@
+package com.bluesky.mainservice.repository.community.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReplyLikesDto {
+    private Long replyId;
+}
