1edc44b0826b894eb40a85fa295049fb1de7ea09
shawnlsj <hortensia08@naver.com>
2022-09-01 23:16:53 +0900

    refactor: Community List<BoardDirectory> 필드 삭제

--- src/main/java/com/bluesky/mainservice/repository/community/domain/Community.java
+++ src/main/java/com/bluesky/mainservice/repository/community/domain/Community.java
@@ -1,7 +1,6 @@
 package com.bluesky.mainservice.repository.community.domain;
 
 import com.bluesky.mainservice.repository.BaseTimeEntity;
-import com.bluesky.mainservice.repository.community.board.domain.BoardDirectory;
 import com.bluesky.mainservice.repository.community.constant.CommunityType;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,8 +8,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Entity
@@ -27,9 +24,6 @@ public class Community extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "community_type", unique = true)
     private CommunityType communityType;
-
-    @OneToMany(mappedBy = "community")
-    private List<BoardDirectory> boardDirectoryList = new ArrayList<>();
 
     public Community(Integer id) {
         this.id = id;
